26340a4572b9d4068ae858110a19fc99ac4b5d63
Caroline Courtney <caroline@carolinecourtney.com>
2016-04-18 16:28:51 -0400

    only approved users can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,15 @@ class  SessionsController < Devise::SessionsController
   # end
 
   def create
-    if (true) #user attempting sign in is approved
-      super
+    self.resource = warden.authenticate!(auth_options)
+    if self.resource.approved?
+      #set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
     else
       flash[:alert] = "You're not approved yet... be patient!"
-      redirect_to root_path
+      redirect_to about_path
     end
   end
 
