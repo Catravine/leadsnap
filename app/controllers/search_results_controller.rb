e9748465b61075116ca4ea94a46b557fbd3fbe03
Caroline Courtney <caroline@carolinecourtney.com>
2016-05-10 14:43:07 -0400

    Can now search for phone numbers unparsed

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -6,8 +6,15 @@ class SearchResultsController < ApplicationController
     if @name_query
       @results = Lead.search_name(@name_query)
     elsif @phone_query
-      @results = Lead.search_phone(@phone_query)
+      @adj_phone_query = @phone_query.scan(/\d/).join
+      @results = Lead.search_phone(phone_parse(@adj_phone_query))
     end
+  end
+
+  private
+
+  def phone_parse(phone)
+    "(" << phone[0..2] << ") " << phone[3..5] << "-" << phone[6..9]
   end
 
 end
