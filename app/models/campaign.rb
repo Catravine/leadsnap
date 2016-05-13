ecb63c1ae0b9bd676dc76d56a74efbf12b7fb9f1
Caroline Courtney <caroline@carolinecourtney.com>
2016-05-13 14:39:28 -0400

    trying to figure out lead association with 'recall' model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,9 @@ class Campaign < ActiveRecord::Base
   has_many :leads, dependent: :destroy
 
   def next_lead(current_lead)
-    return leads.where(killed: false, day_lead: true, disconnected: false).first if current_lead.day_lead?
-    leads.where(killed: false, day_lead: false, disconnected: false, source_code: current_lead.source_code).first
+    unreachead_leads = leads
+    return unreachead_leads.where(killed: false, day_lead: true, disconnected: false).first if current_lead.day_lead?
+    unreachead_leads.where(killed: false, day_lead: false, disconnected: false, source_code: current_lead.source_code).first
   end
 
 end
