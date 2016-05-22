02f60503fc089d74510e84ab7c136d76bfe24c9b
Caroline Courtney <caroline@carolinecourtney.com>
2016-05-22 14:21:30 -0400

    Eastern Time zone and refactoring 5/20 meeting

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,12 +1,15 @@
 module CampaignsHelper
 
-  def all_leads(campaign, *code)
-    return Lead.where(campaign_id: campaign, source_code: code) if code.present?
-    Lead.where(campaign_id: campaign)
+  def all_leads(campaign, code=nil)
+    unreached_leads = Lead.where.not(:id => Recall.select(:lead_id))
+    unreached_leads = unreached_leads.where.not(:id => Sale.select(:lead_id))
+    unreached_leads = unreached_leads.where.not(:id => No.select(:lead_id))
+    return unreached_leads.where(campaign_id: campaign, source_code: code) if code
+    unreached_leads.where(campaign_id: campaign)
   end
 
-  def all_valid_leads(campaign, *code)
-    return all_leads(campaign, code).where(killed: false, disonnected: false, day_lead: false) if code.present?
+  def all_valid_leads(campaign, code=nil)
+    return all_leads(campaign, code).where(killed: false, disonnected: false, day_lead: false) if code
     all_leads(campaign).where(killed: false, disconnected: false, day_lead: false)
   end
 
@@ -18,8 +21,8 @@ module CampaignsHelper
     all_day_leads(campaign).count
   end
 
-  def amt_leads(campaign, *code)
-    return all_valid_leads(campaign).where(source_code: code).count if code.present?
+  def amt_leads(campaign, code=nil)
+    return all_valid_leads(campaign).where(source_code: code).count if code
     all_valid_leads(campaign).count
   end
 
