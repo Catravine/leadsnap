a7609634d9aab9f22af5352218e6f8cc45720767
Caroline Courtney <caroline@carolinecourtney.com>
2016-05-22 15:31:33 -0400

    Lead scopes instead of Campaign helper methods

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,31 +1,5 @@
 module CampaignsHelper
 
-  def all_leads(campaign, code=nil)
-    unreached_leads = Lead.where.not(:id => Recall.select(:lead_id))
-    unreached_leads = unreached_leads.where.not(:id => Sale.select(:lead_id))
-    unreached_leads = unreached_leads.where.not(:id => No.select(:lead_id))
-    return unreached_leads.where(campaign_id: campaign, source_code: code) if code
-    unreached_leads.where(campaign_id: campaign)
-  end
-
-  def all_valid_leads(campaign, code=nil)
-    return all_leads(campaign, code).where(killed: false, disonnected: false, day_lead: false) if code
-    all_leads(campaign).where(killed: false, disconnected: false, day_lead: false)
-  end
-
-  def all_day_leads(campaign)
-    all_leads(campaign).where(day_lead: true, killed: false, disconnected: false)
-  end
-
-  def amt_day_leads(campaign)
-    all_day_leads(campaign).count
-  end
-
-  def amt_leads(campaign, code=nil)
-    return all_valid_leads(campaign).where(source_code: code).count if code
-    all_valid_leads(campaign).count
-  end
-
   def sources(campaign)
     Lead.where(campaign_id: campaign.id).pluck(:source_code).uniq
   end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -7,7 +7,14 @@ class Lead < ActiveRecord::Base
   has_one :sale, dependent: :destroy
   has_one :no, dependent: :destroy
 
+  # Default order in use is to serve them in order of last dialed
   default_scope { order("leads.last_dialed ASC") }
+
+  # Scopes to find different sets of leads
+  scope :by_source_code, -> (code) { where(source_code: code) }
+  scope :unreached, -> { where.not(:id => Recall.select(:lead_id)).where.not(:id => Sale.select(:lead_id)).where.not(:id => No.select(:lead_id))} 
+  scope :valid, -> { where(killed: false, disconnected: false) }
+  scope :day_leads, -> { where(day_lead: true) }
 
   delegate :name, to: :campaign, prefix: true
   delegate :notes, to: :campaign, prefix: true
