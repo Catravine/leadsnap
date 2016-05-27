7f3f62c0ae17c2b1af2d938d9e5c43dd50627065
Caroline Courtney <caroline@carolinecourtney.com>
2016-05-27 14:49:36 -0400

    Basic user totals and goals rundown

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Default order alpha by nickname
+  default_scope { order("users.nickname ASC") }
+
   before_save :set_nickname
   before_save { self.role ||= :standard }
 
@@ -12,6 +15,11 @@ class User < ActiveRecord::Base
   has_many :nos, dependent: :destroy
 
   enum role: [:standard, :manager, :admin]
+
+  # Week-to-date running total of sales amounts
+  def week_total
+    Sale.where(date: Time.now.beginning_of_week..Time.now, user: self).sum("amount")
+  end
 
   private
 
