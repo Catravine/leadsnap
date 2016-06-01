9338e34cb251fcb9a5d5138e5b39cd13c11f4197
Caroline Courtney <caroline@carolinecourtney.com>
2016-06-01 17:00:00 -0400

    Goal creating fix

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -3,7 +3,7 @@ class Admin::GoalsController < Admin::AdminController
   def index
     @goals = Goal.all
   end
-  
+
   def show
     @goal = Goal.find(params[:id])
   end
@@ -16,11 +16,10 @@ class Admin::GoalsController < Admin::AdminController
     @goal = Goal.new(goal_params)
     if @goal.save
       flash[:notice] = "Goal saved."
-      redirect_to admin_path
     else
       flash[:alert] = "There was an error saving this goal.  Please try again."
-      redirect_to admin_goals_path
     end
+    redirect_to admin_goals_path
   end
 
   def destroy
