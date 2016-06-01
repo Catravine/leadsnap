6db95d8df2bf5176a7b188ca77734d6fbb8e60b4
Caroline Courtney <caroline@carolinecourtney.com>
2016-06-01 16:49:26 -0400

    Create or delete your own goals

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -3,5 +3,36 @@ class Admin::GoalsController < Admin::AdminController
   def index
     @goals = Goal.all
   end
+  
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      flash[:notice] = "Goal saved."
+      redirect_to admin_path
+    else
+      flash[:alert] = "There was an error saving this goal.  Please try again."
+      redirect_to admin_goals_path
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to admin_goals_path
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:amount, :weekly, :individual)
+  end
 
 end
